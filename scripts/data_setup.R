33bdda1a6bc5057e747230dff10a1e6c58ce76d4
Gerard Rocher-Ros <gerardrocher@gmail.com>
2025-07-03 11:11:56 +0200

    added gitignore

--- scripts/data_setup.R
+++ scripts/data_setup.R
@@ -1,3 +1,9 @@
-options(timeout = 200)
+options(timeout = 500)
 
-download.file(url, destfile = "downloded_github.zip")
+
+url <- "https://zenodo.org/records/13990674/files/prepared%20data.zip?download=1"
+
+
+download.file(url, destfile = "prepared data.zip")
+
+unzip("prepared data.zip")
